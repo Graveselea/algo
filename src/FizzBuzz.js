7eb8392b17f7f09d2f3e1ddc35338c90a50e0f4e
graveselea <gravagna.elea@gmail.com>
2025-06-18 17:11:55 +0200

    Fizz buzz project

---
+++ src/FizzBuzz.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+export default function FizzBuzz() {
+  const [number, setNumber] = useState(100);
+
+  const handleChange = (e) => {
+    setNumber(Number(e.target.value));
+  };
+
+  const fizzBuzzItems = [];
+
+  for (let i = 1; i <= number; i++) {
+    let value = "";
+    let id = "";
+
+    if (i % 15 === 0) {
+      value = "FizzBuzz";
+      id = "fizzbuzz";
+    } else if (i % 3 === 0) {
+      value = "Fizz";
+      id = "fizz";
+    } else if (i % 5 === 0) {
+      value = "Buzz";
+      id = "buzz";
+    } else {
+      value = i;
+      id = "number";
+    }
+
+    fizzBuzzItems.push(
+      <li key={i} id={id}>
+        {value}
+      </li>
+    );
+  }
+
+  return (
+    <>
+      <h1>Fizz Buzz</h1>
+      <label>
+        Jusqu'à combien ?{" "}
+        <input type="number" value={number} onChange={handleChange} />
+      </label>
+      <div>
+        <ul id="numbers-list">{fizzBuzzItems}</ul>
+      </div>
+    </>
+  );
+}
